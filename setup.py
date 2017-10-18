e0ec0eee5b0269473e2100c0b7c4d3ccf4face71
Mubaris NK <mubarishassannk@gmail.com>
2017-10-18 21:28:38 +0530

    Add to PyPi

--- setup.py
+++ setup.py
@@ -5,6 +5,11 @@ setup(
     name='fez',
     version='0.1',
     packages=find_packages(),
+    author='Mubaris NK',
+    author_email = 'mubarishassannk@gmail.com',
+    url='https://github.com/mubaris/Fez',
+    keywords = ['translate', 'google'],
+    download_url='https://github.com/mubaris/Fez/archive/0.1.tar.gz',
     install_requires=[
         'googletrans==2.2.0',
         'tableprint==0.7.0',
